50aecd1fecc246063c1cdd382228a018fb781dc4
MartinKasak <martin.kasak11@gmail.com>
2023-07-03 17:09:47 +0300

    Manual pokemon name works

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -2,6 +2,7 @@
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
 import { Input } from "./ui/input";
+import { PokemonCard } from "./pokemon-card";
 
 export function PokemonGrid() {
   const [searchText, setSearchText] = useState("");
@@ -12,15 +13,20 @@ export function PokemonGrid() {
         <h3 className="text-2xl py-6 text-center">Otsi Pokemon</h3>
         <div className="grid w-full max-w-sm item-center gap-1.5">
           <Label htmlFor="pokemonName">Pokemoni nimi</Label>
-          <Input 
-            type="text" 
-            value={searchText} 
+          <Input
+            type="text"
+            value={searchText}
             autoComplete="off"
             id="pokemonName"
             placeholder="Pikatchu, Arboc, Smogon, ..."
-            onChange={(e)=> setSearchText(e.target.value)}
-            />
+            onChange={(e) => setSearchText(e.target.value)}
+          />
         </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <PokemonCard name="arboc" />
+
       </div>
     </>
   );
